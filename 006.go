d2b46fcad20c116ffaa555913458551fefec7762
Moh <ahoodashti2000@gmail.com>
2024-11-08 12:06:31 -0800

    اضافه کردن فایل

---
+++ 006.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	number1 := 10
+	number2 := 7
+	number1 = number1 + number2
+	fmt.Println(number1)
+}
---
+++ 010.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	X := 3
+
+	Y := 4
+
+	fmt.Println(X) == (Y)
+
+}
